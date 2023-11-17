905b43c63a8cf5e8776838e6cf86d715f1e842e2
Shane Orend <sorend95@outlook.com>
2023-11-16 23:52:08 -0500

    minor updates
    added new views and updated all cars to test out closing modal from any where in modal and adding view next and previous image logic

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,65 +6,91 @@ const router = express.Router();
 
 // Home Page
 router.get('/', (req, res) => {
-  res.render('home', { recPhotos });
+    res.render('home', { recPhotos });
 });
 
 
 // Sample photos data (replace this with your actual data source)
 const photos = [
-  { imageUrl: '/images/photo1.jpg', description: 'Description 1' },
-  { imageUrl: '/images/c4s.jpg', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-   { imageUrl: '/images/_DSC0104.jpg', description: 'JDM Oddessey' },
-   { imageUrl: '/images/_DSC0815.jpg', description: 'Japan VW Bus' },
-   { imageUrl: '/images/_DSC0569.jpg', description: '' },
-   { imageUrl: '/images/_DSC0489-2.jpg', description: '' },
-   { imageUrl: '/images/_DSC0430.jpg', description: '' },
-   { imageUrl: '/images/_DSC0408.jpg', description: '' },
-   { imageUrl: '/images/_DSC0386.jpg', description: '' },
-   { imageUrl: '/images/_DSC0334.jpg', description: '' },
-   { imageUrl: '/images/_DSC0311.jpg', description: '' },
-   { imageUrl: '/images/_DSC0307.jpg', description: '' },
-   { imageUrl: '/images/_DSC0246-2.jpg', description: '' },
-   { imageUrl: '/images/_DSC0225-2.jpg', description: '' },
-   { imageUrl: '/images/_DSC0210.jpg', description: '' },
-   { imageUrl: '/images/_DSC0203.jpg', description: '' },
-   { imageUrl: '/images/_DSC0155-Pano.jpg', description: '' },
-   { imageUrl: '/images/_DSC0102.jpg', description: '' },
-   { imageUrl: '/images/_DSC0100.jpg', description: '' },
-   { imageUrl: '/images/_DSC0096.jpg', description: '' },
-   { imageUrl: '/images/_DSC0092.jpg', description: '' },
-   { imageUrl: '/images/_DSC0087.jpg', description: '' },
-   { imageUrl: '/images/_DSC0086.jpg', description: '' },
-   { imageUrl: '/images/_DSC0084.jpg', description: '' },
-   { imageUrl: '/images/_DSC0079.jpg', description: '' },
-   { imageUrl: '/images/_DSC0078.jpg', description: '' },
-   { imageUrl: '/images/_DSC0071.jpg', description: '' },
-   { imageUrl: '/images/_DSC0070.jpg', description: '' },
-   { imageUrl: '/images/_DSC0069-2.jpg', description: '' },
-   { imageUrl: '/images/_DSC0067.jpg', description: '' },
-   { imageUrl: '/images/_DSC0064.jpg', description: '' },
-   { imageUrl: '/images/_DSC0063.jpg', description: '' },
-   { imageUrl: '/images/_DSC0056-2.jpg', description: '' },
-   { imageUrl: '/images/_DSC0054.jpg', description: '' },
+    { imageUrl: '/images/photo1.jpg', description: 'Description 1' },
+    { imageUrl: '/images/c4s.jpg', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+    { imageUrl: '/images/_DSC0104.jpg', description: 'JDM Oddessey' },
+    { imageUrl: '/images/_DSC0815.jpg', description: 'Japan VW Bus' },
+    { imageUrl: '/images/_DSC0569.jpg', description: '' },
+    { imageUrl: '/images/_DSC0489-2.jpg', description: '' },
+    { imageUrl: '/images/_DSC0430.jpg', description: '' },
+    { imageUrl: '/images/_DSC0408.jpg', description: '' },
+    { imageUrl: '/images/_DSC0386.jpg', description: '' },
+    { imageUrl: '/images/_DSC0334.jpg', description: '' },
+    { imageUrl: '/images/_DSC0311.jpg', description: '' },
+    { imageUrl: '/images/_DSC0307.jpg', description: '' },
+    { imageUrl: '/images/_DSC0246-2.jpg', description: '' },
+    { imageUrl: '/images/_DSC0225-2.jpg', description: '' },
+    { imageUrl: '/images/_DSC0210.jpg', description: '' },
+    { imageUrl: '/images/_DSC0203.jpg', description: '' },
+    { imageUrl: '/images/_DSC0155-Pano.jpg', description: '' },
+    { imageUrl: '/images/_DSC0102.jpg', description: '' },
+    { imageUrl: '/images/_DSC0100.jpg', description: '' },
+    { imageUrl: '/images/_DSC0096.jpg', description: '' },
+    { imageUrl: '/images/_DSC0092.jpg', description: '' },
+    { imageUrl: '/images/_DSC0087.jpg', description: '' },
+    { imageUrl: '/images/_DSC0086.jpg', description: '' },
+    { imageUrl: '/images/_DSC0084.jpg', description: '' },
+    { imageUrl: '/images/_DSC0079.jpg', description: '' },
+    { imageUrl: '/images/_DSC0078.jpg', description: '' },
+    { imageUrl: '/images/_DSC0071.jpg', description: '' },
+    { imageUrl: '/images/_DSC0070.jpg', description: '' },
+    { imageUrl: '/images/_DSC0069-2.jpg', description: '' },
+    { imageUrl: '/images/_DSC0067.jpg', description: '' },
+    { imageUrl: '/images/_DSC0064.jpg', description: '' },
+    { imageUrl: '/images/_DSC0063.jpg', description: '' },
+    { imageUrl: '/images/_DSC0056-2.jpg', description: '' },
+    { imageUrl: '/images/_DSC0054.jpg', description: '' },
 ];
 const recPhotos = photos.slice(-3);
 
+const portfolioPhotos = [
+    { imageUrl: '/images/_DSC0489-2.jpg', description: 'drift' },
+    { imageUrl: '/images/_DSC0489-2.jpg', description: 'japan' },
+    { imageUrl: '/images/_DSC0489-2.jpg', description: 'cars' },
+    { imageUrl: '/images/_DSC0489-2.jpg', description: 'thisIsai' },
+];
+
 // Grid Page Route Handler
 router.get('/grid', (req, res) => {
-  res.render('grid', { photos });
+    res.render('grid', { portfolioPhotos });
+});
+
+//I think these could be handled in the same way the navbar is each just filled with new content but sharing the layou
+router.get('/grid/japan', (req, res) => {
+    res.render('japan', { photos });
+});
+router.get('/grid/surfing', (req, res) => {
+    res.render('surf', { photos });
+});
+router.get('/grid/aiContent', (req, res) => {
+    res.render('aiPage', { photos });
+});
+router.get('/grid/cars', (req, res) => {
+    res.render('cars', { photos });
+});
+router.get('/grid/cars/drifting', (req, res) => {
+    res.render('drift', { photos });
+});
+//everything else
+router.get('/grid/cars/all', (req, res) => {
+    res.render('allCars', { photos });
 });
 
 
 // Contact Page (Placeholder route)
 router.get('/contact', (req, res) => {
-  res.render('contact'); // Placeholder message, replace with actual contact content
+    res.render('contact'); // Placeholder message, replace with actual contact content
 });
 
 
 // Blog Page
 router.get('/blog', (req, res) => {
-  res.render('blogHome', { recPhotos });
+    res.render('blogHome', { recPhotos });
 });
 module.exports = router;
-
-
