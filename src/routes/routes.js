c2eb2499f36203f186984fca17bd9586b05743af
Shane Orend <sorend95@outlook.com>
2023-11-17 00:55:50 -0500

    update new views
    added images to some of the new views and added home page tab icon. Need to review having view image array populate off one array based on desc.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,9 +11,7 @@ router.get('/', (req, res) => {
 
 
 // Sample photos data (replace this with your actual data source)
-const photos = [
-    { imageUrl: '/images/photo1.jpg', description: 'Description 1' },
-    { imageUrl: '/images/c4s.jpg', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+const japanPhotos = [
     { imageUrl: '/images/_DSC0104.jpg', description: 'JDM Oddessey' },
     { imageUrl: '/images/_DSC0815.jpg', description: 'Japan VW Bus' },
     { imageUrl: '/images/_DSC0569.jpg', description: '' },
@@ -47,14 +45,33 @@ const photos = [
     { imageUrl: '/images/_DSC0056-2.jpg', description: '' },
     { imageUrl: '/images/_DSC0054.jpg', description: '' },
 ];
-const recPhotos = photos.slice(-3);
+const recPhotos = japanPhotos.slice(-3);
+
+const genCarPhotos =[
+{ imageUrl: '/images/JSO_1764.jpg', description: 'AudiRS6,S200,cars' },
+{ imageUrl: '/images/JSO_1983.jpg', description: 'S200,cars' },
+{ imageUrl: '/images/JSO_2235.jpg', description: 'FocusSt,cars' },
+  ]
 
 const portfolioPhotos = [
-    { imageUrl: '/images/_DSC0489-2.jpg', description: 'drift' },
+    { imageUrl: '/images/JSO_4730.jpg', description: 'drift' },
     { imageUrl: '/images/_DSC0489-2.jpg', description: 'japan' },
-    { imageUrl: '/images/_DSC0489-2.jpg', description: 'cars' },
-    { imageUrl: '/images/_DSC0489-2.jpg', description: 'thisIsai' },
+    { imageUrl: '/images/JSO_1764.jpg', description: 'cars' },
+    { imageUrl: '/images/photo1.jpg', description: 'thisIsai' },
 ];
+
+const driftPhotos = [
+    { imageUrl: '/images/JSO_4774.jpg', description: 'drift,s14' },
+    { imageUrl: '/images/JSO_4730.jpg', description: 'drift,mk5supra' },
+    { imageUrl: '/images/JSO_4598.jpg', description: 'drift,ferrari' },
+
+];
+
+const aiCon = [
+    { imageUrl: '/images/photo1.jpg', description: 'Description 1' },
+    { imageUrl: '/images/c4s.jpg', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+
+]
 
 // Grid Page Route Handler
 router.get('/grid', (req, res) => {
@@ -63,23 +80,23 @@ router.get('/grid', (req, res) => {
 
 //I think these could be handled in the same way the navbar is each just filled with new content but sharing the layou
 router.get('/grid/japan', (req, res) => {
-    res.render('japan', { photos });
+    res.render('japan', { japanPhotos });
 });
 router.get('/grid/surfing', (req, res) => {
     res.render('surf', { photos });
 });
 router.get('/grid/aiContent', (req, res) => {
-    res.render('aiPage', { photos });
+    res.render('aiPage', { aiCon });
 });
 router.get('/grid/cars', (req, res) => {
     res.render('cars', { photos });
 });
 router.get('/grid/cars/drifting', (req, res) => {
-    res.render('drift', { photos });
+    res.render('drift', { driftPhotos });
 });
 //everything else
 router.get('/grid/cars/all', (req, res) => {
-    res.render('allCars', { photos });
+    res.render('allCars', { genCarPhotos });
 });
 
 
